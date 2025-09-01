98dab843519f7b8c16b32281df3f045eaf73e94b
Ilia Bykov <fagotist96@icloud.com>
2025-09-02 00:38:19 +0300

    кнопки не меняют стили при наведении

---
+++ index.js
@@ -0,0 +1,31 @@
+//1
+let a = 10;
+alert(a);
+
+a = 20;
+alert(a);
+
+//2
+const year = 2007;
+alert(year);
+
+//3
+const creator = 'Brendan Eich';
+alert(creator);
+
+//4
+const ten = 10;
+const two = 2;
+alert(ten + two)
+alert(ten - two)
+alert(ten * two)
+alert(ten / two)
+
+//5
+const result = 2 ** 5;
+alert(result);
+
+//6
+a = 9;
+const b = 2;
+alert(a % 2);
