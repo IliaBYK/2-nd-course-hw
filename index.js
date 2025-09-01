0794cc87b49fb66ec43ba55587dc80418eb5984c
Ilia Bykov <fagotist96@icloud.com>
2025-09-02 00:53:06 +0300

    задание по js

--- index.js
+++ index.js
@@ -29,3 +29,45 @@ alert(result);
 a = 9;
 const b = 2;
 alert(a % 2);
+
+//7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+//8
+const age = prompt('Сколько вам лет?')
+alert(age)
+
+//9
+const user = {
+  name: 'Иван', 
+  age: 20, 
+  isAdmin: true
+}
+
+//10
+const nameUser = promt('Как вас зовут?')
+alert(`Привет, ${nameUser}`)
+
+//Дополнительное
+const number = promt('Загадайте любое число')
+
+const numberOne = number * 2;
+alert(`Удвоенное загаданное число: ${numberOne}`);
+
+const numberTwo = numberOne + 10;
+alert(`К удвоенному числу прибавили 10: ${numberTwo}`);
+
+const numberThree = numberTwo / 2;
+alert(`Полученное число разделили на 2: ${numberThree}`);
+
+const numberFour = numberThree - number;
+alert(`Из результата вычли первоначальное загаданное число: ${numberFour}`)
+
+alert('Ответ: 5')
