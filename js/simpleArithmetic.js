16577abdb7057e9ed731e981523deb011ebb5025
Ilia Bykov <fagotist96@icloud.com>
2025-09-06 22:44:09 +0300

    округляю в меньшу сторону, второй число всегда меньше первого

--- js/simpleArithmetic.js
+++ js/simpleArithmetic.js
@@ -4,7 +4,11 @@ const simpleArithmetic = () => {
   const operations = ['+', '-', '*', '/']
   const randomOperation = operations[Math.floor(Math.random() * operations.length)];
   const randomOne = Math.floor(Math.floor(Math.random() * 10) + 1);
-  const randomTwo = Math.floor(Math.floor(Math.random() * 10) + 1);
+  let randomTwo = Math.floor(Math.floor(Math.random() * 10) + 1);
+
+  if (randomTwo > randomOne) {
+    randomTwo = Math.floor(Math.random() * 10) + 1;
+  }
   let userGuess;
 
   function calculate(operator, num1, num2) {
@@ -25,28 +29,28 @@ const simpleArithmetic = () => {
   alert('Добро пожаловать в игру Простая арифметика!');
   alert(`Вам будут представлены арифметические задачи, 
   попробуйте решить их и написать ответ в поле ввода;
-  результат можно округлить`);
+  результат можно округлить в меньшую сторону`);
   alert([randomOne, randomOperation, randomTwo].join(''))
     
   do {
     userGuess = parseInt(prompt('Введите ваш ответ: '), 10);
 
-    if(!userGuess) {
+    if(!userGuess && userGuess !== 0) {
       alert('Вы завершили игру');
       break;
     }
 
-    if (userGuess !== Math.round(calculate(randomOperation, randomOne, randomTwo))) {
+    if (userGuess !== Math.floor(calculate(randomOperation, randomOne, randomTwo)) || userGuess === 0) {
       alert(`К сожалению, вы ошиблись, попробуйте еще раз
       Вот задача:  ${[randomOne, randomOperation, randomTwo].join('')}`);
       continue;
     }
 
-    if (userGuess === Math.round(calculate(randomOperation, randomOne, randomTwo))) {
+    if (userGuess === Math.floor(calculate(randomOperation, randomOne, randomTwo))) {
       alert(`Поздравляем! Вы решили задачу.`)
     }
     
-  } while (userGuess !== Math.round(calculate(randomOperation, randomOne, randomTwo)));
+  } while (userGuess !== Math.floor(calculate(randomOperation, randomOne, randomTwo)));
 }
 
 buttonSimple.addEventListener('click', simpleArithmetic)
